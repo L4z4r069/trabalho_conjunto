088aed9b94ff8080d7c859e61c1f2264b901cb8a
JardimG <joao.2020316204@aluno.iffar.edu.br>
2024-03-11 16:45:39 -0300

    Criei o formulario

---
+++ form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário de Cadastro</title>
+</head>
+<body>
+    <form action="cadastrar.php" method="post">
+        ID Produto: <input type="number" name="id_produto"><br>
+        Nome: <input type="text" name="n1"><br>
+        Quantidade: <input type="password" name="senha"><br>
+        <input type="submit" value="Enviar"><br>
+    </form>
+</body>
+</html>
