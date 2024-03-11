9e2895583289505f9875c01316ab6a645d9ea6d4
L4z4r069 <lazaro.2022315968@aluno.iffar.edu.br>
2024-03-11 16:46:36 -0300

    index

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crud</title>
+</head>
+<body>
+    <form action="crud.php" method="post">
+        <tr>
+            <th> Nome </th>
+            <th> Quantidade </th>
+        </tr>
+
+</form>
+</body>
+</html>
